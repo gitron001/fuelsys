fe7e571c808df1de39e108806d23708495bffa81
Ideal Bakija <ideal.bakija@gmail.com>
2019-08-31 21:08:52 +0200

    small fix viti

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -99,17 +99,21 @@ class Transaction extends Model
         $rfid = unpack('i', $rfid)[1];
 
         $user = Users::where("rfid", $rfid)->where('status', 1)->first();
-
-        if(!is_null($user->company->id) && $user->company->has_limit == 1){
-            $company = Company::find( $user->company->id );
-            $company->limit_left -= $transaction->money;
-            $company->save();
-        }elseif($user->has_limit == 1){
-            $user->limit_left -= $transaction->money;
-            $user->save();
-        }
+		if(isset($user->id)){
+			if(!is_null($user->company->id) && $user->company->has_limit == 1){
+				$company = Company::find( $user->company->id );
+				$company->limit_left -= $transaction->money;
+				$company->save();
+			}elseif($user->has_limit == 1){
+				$user->limit_left -= $transaction->money;
+				$user->save();
+			}
         //Query the rfid ID from the RFID table
-        $transaction->user_id = $user->id;
+			$transaction->user_id = $user->id;
+		}else{
+			$transaction->user_id = $rfid;
+			echo $rfid;
+		}
 
         $transaction->ctype = $response[34];
 
