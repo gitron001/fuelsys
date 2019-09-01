2273d821c9c55cae81c37906e9d029ffe979ae03
Ideal Bakija <ideal.bakija@gmail.com>
2019-09-01 08:14:03 +0200

    small fix on payment controller

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -126,7 +126,7 @@ class PaymentsController extends Controller
         /*$msg = "Payment Print not Succesful";
 		*/
         try {
-            self::printFunction($payments->id);
+            //self::printFunction($payments->id);
         } catch (Exception $e) {
            $msg = "Payment Print NOT Succesful";
         }
