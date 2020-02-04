fc5f2703ffc4069cda5edc636546369c05d597ed
Orges Thaqi <orgesthaqi96@gmail.com>
2020-02-04 16:10:11 +0100

    fix export transaction .txt

--- app/Console/Commands/ExportTransactions.php
+++ app/Console/Commands/ExportTransactions.php
@@ -44,27 +44,26 @@ class ExportTransactions extends Command
         $tr_location    = $company->transaction_location;
 
 
-        if($company->print_transaction == 1){
+        if($company->print_transaction == 1 && !empty($tr_location)){
 
             $transactions = Transaction::select(DB::RAW('products.name as product'),
             'transactions.price', 'transactions.lit','transactions.money','transactions.id','transactions.channel_id','transactions.sl_no','transactions.tr_no','transactions.product_id','transactions.lit','transactions.price','transactions.money','transactions.created_at')
                 ->leftJoin('products', 'products.id', '=', 'transactions.product_id')
-                ->orderBy('transactions.id','DESC')
-                ->first();
+                ->where('transactions.printed',0)
+                ->get();
 
-            if(empty($tr_location) || $tr_location == NULL){
-                $tr_location = storage_path()."/logs/Trans_".$transactions['id']."_".date("d-m-Y").".txt";
-            }else{
-                $tr_location = $tr_location."/Trans_".$transactions['id']."_".date("d-m-Y").".txt";
+            foreach($transactions as $transaction){
+
+                $fp = fopen($tr_location."/Trans_".$transaction['id']."_".date("d-m-Y").".txt", "a");
+
+                fwrite($fp, 'Kanali, Doreza, SeqNumber, GradeId, Malli, Sasia, Cmimi, Shuma, ZbritjaVlere, Zbritja%,  ShumaMeZbritje, Data, Totali, StartTime, FinishTime;' .PHP_EOL);
+
+                fwrite($fp, $transaction['channel_id'].','.$transaction['sl_no'].','.$transaction['tr_no'].','.$transaction['product_id'].','.$transaction['product'].','.$transaction['lit'].','.$transaction['price'].','.$transaction['money'].',0,0,'.$transaction['money'].','.date("d/m/Y h:i:s").',0,'.date("d/m/Y h:i:s",strtotime($transaction['created_at'])).','.date("d/m/Y h:i:s",strtotime($transaction['created_at'])).';');
+
+                Transaction::where('id',$transaction['id'])->update(['printed' => 1]);
+
+                fclose($fp);
             }
-
-            $fp = fopen($tr_location, "a");
-
-            fwrite($fp, 'Kanali, Doreza, SeqNumber, GradeId, Malli, Sasia, Cmimi, Shuma, ZbritjaVlere, Zbritja%,  ShumaMeZbritje, Data, Totali, StartTime, FinishTime;' .PHP_EOL);
-
-            fwrite($fp, $transactions['channel_id'].','.$transactions['sl_no'].','.$transactions['tr_no'].','.$transactions['product_id'].','.$transactions['product'].','.$transactions['lit'].','.$transactions['price'].','.$transactions['money'].',0,0,'.$transactions['money'].','.date("d/m/Y h:i:s").',0,'.date("d/m/Y h:i:s",strtotime($transactions['created_at'])).','.date("d/m/Y h:i:s",strtotime($transactions['created_at'])).';');
-
-            fclose($fp);
         }
     }
 }
