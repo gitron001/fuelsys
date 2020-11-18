550e8901fb5ce70e44daf8606013415144840ec8
Ideal Bakija <ideal.bakija@gmail.com>
2020-11-18 15:16:09 +0100

    small fix in CardService

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -79,7 +79,7 @@ class Handler extends ExceptionHandler
 					$emails = ['ideal.bakija@gmail.com','orgesthaqi96@gmail.com'];
                     $m->from('ideal.bakija@bakija.com', 'Fuel System');
 
-                    $m->to($emails)->subject("Fatal error - NESIM BAKIJA");
+                    $m->to($emails)->subject("Fatal error - DARDANIA(661800870)");
                 });
             } catch (\Exception $e) {
 
