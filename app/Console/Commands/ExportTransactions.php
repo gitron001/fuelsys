4f23b97a4d82e5dfb8cf2199b49fae517abe9cc8
Ideal Bakija <ideal.bakija@gmail.com>
2023-12-12 14:43:09 +0100

    working on rfid redears with kilometers

--- app/Console/Commands/ExportTransactions.php
+++ app/Console/Commands/ExportTransactions.php
@@ -47,7 +47,7 @@ class ExportTransactions extends Command
         if($company->print_transaction == 1 && !empty($tr_location)){
 
             $transactions = Transaction::select(DB::RAW('products.name as product'),DB::RAW('products.price as product_price'),DB::RAW('u2.name as bonus_user'),DB::RAW('u1.name as staff_name'),DB::RAW('u2.rfid as bonus_rfid'),DB::RAW('u1.rfid as staff_rfid'),
-            'transactions.price', 'transactions.lit','transactions.money','transactions.id','transactions.channel_id','transactions.sl_no','transactions.tr_no','transactions.product_id','transactions.lit','transactions.price','transactions.money','transactions.created_at','transactions.bonus_user_id','transactions.user_id','transactions.channel_id')
+            'transactions.price', 'transactions.lit','transactions.kilometers', 'transactions.money','transactions.id','transactions.channel_id','transactions.sl_no','transactions.tr_no','transactions.product_id','transactions.lit','transactions.price','transactions.money','transactions.created_at','transactions.bonus_user_id','transactions.user_id','transactions.channel_id')
                 ->leftJoin('products', 'products.pfc_pr_id', '=', 'transactions.product_id')
                 ->leftJoin('users AS u1', 'u1.id', '=', 'transactions.user_id')
                 ->leftJoin('users AS u2', 'u2.id', '=', 'transactions.bonus_user_id')
@@ -58,11 +58,11 @@ class ExportTransactions extends Command
 
             if(!file_exists($tr_location."/export-transactions.txt")){
                 $fp = fopen($tr_location."/export-transactions.txt", "a");
-                fwrite($fp, 'ID_PERSHT;DATAORA;KODART;ARTIKULLI;SASIA;CMIMI;REZERVUAR;RFID;NrKuponit' .PHP_EOL);
+                fwrite($fp, 'ID_PERSHT;DATAORA;KODART;ARTIKULLI;SASIA;CMIMI;REZERVUAR;RFID;Kilometrat;NrKuponit' .PHP_EOL);
 
                 foreach($transactions as $transaction){
 
-                    fwrite($fp, $transaction['id'].';'.date("d.m.Y H:i:s",strtotime($transaction['created_at'])).';'.$transaction['product_id'].';'.$transaction['product'].';'.$transaction['lit'].';'.$transaction['price'].';0;'.$transaction['staff_rfid'].';'.$transaction['id'].PHP_EOL);
+                    fwrite($fp, $transaction['id'].';'.date("d.m.Y H:i:s",strtotime($transaction['created_at'])).';'.$transaction['product_id'].';'.$transaction['product'].';'.$transaction['lit'].';'.$transaction['price'].';0;'.$transaction['staff_rfid'].';'.$transaction['kilometers'].';'.$transaction['id'].PHP_EOL);
 
                     Transaction::where('id',$transaction['id'])->update(['printed' => 1]);
                 }
